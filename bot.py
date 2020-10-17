24987d0aa66864626957b31a827d5fde0d2e0055
Joao Matheus Francolin <joaomatheusfrancolin@MacBook-Pro.local>
2020-10-17 12:33:13 -0300

    change ftx endpoint to /funding_rates

--- bot.py
+++ bot.py
@@ -8,6 +8,8 @@ import telegram
 import config
 import time
 
+from dateutil import tz
+
 class Bot:
 
     def __init__(self):
@@ -22,8 +24,6 @@ class Bot:
         self.OUTPUT_NUMBER = env.int('OUTPUT_NUMBER')
         self.OUTPUT_THRESHOLD = env.int('OUTPUT_THRESHOLD')
         self.LIST_OF_FUTURES = env.list('LIST_OF_FUTURES')
-        if self.LIST_OF_FUTURES[0].lower() == 'all':
-            self.LIST_OF_FUTURES = self.get_futures()
         
         # attempt to set up telegram client
         try:
@@ -33,68 +33,76 @@ class Bot:
             self.tg_client = None
             self.TELEGRAM_CHAT_ID = None
 
+        # time of lastest queried rate
+        self.rate_time = None
+
     def start(self):
-        # keep track of starting time
-        self.ts = int(time.time())
+
         # loop forever
         while True:
-            stats = self.get_stats()
-            stats = self.filter_stats(stats)
-            report = self.create_report(stats)
+            rates = self.get_rates()
+            rates = self.filter_rates(rates)
+            report = self.create_report(rates)
             self.post_report(report)
-
-            # self.ts += self.UPDATE_DELAY
             time.sleep(self.UPDATE_DELAY)
 
-    def get_futures(self):
-        names = []
-        resp = self.ftx_client.get('/futures')
-        for i in range(len(resp)):
-            names.append(resp[i]['name'])
-        return list(set(names))
+    def get_rates(self):
+        resp = self.ftx_client.get('/funding_rates')
+        rates = pd.DataFrame(resp)
+        return rates.sort_values(by=['rate'], ascending=False).reset_index(drop=True)
+ 
+    def filter_rates(self, rates):
 
-    def get_stats(self):
-        stats = []
-        for future in self.LIST_OF_FUTURES:
-            resp = self.ftx_client.get(f'/futures/{future}/stats')
-            pdb.set_trace()
-            try:
-                stat = {'future':future, 'rate':resp['nextFundingRate']}
-                stats.append(stat)
-            except:
-                continue
-        stats = pd.DataFrame(stats)
-        return stats.sort_values(by=['rate'], ascending=False).reset_index(drop=True)
+        # filter by latest time
+        max_utc = max(rates['time'])
+        rates = rates[rates['time'] == max_utc]
 
-    def filter_stats(self, stats):
-        if len(stats) > 2 * self.OUTPUT_NUMBER:
-            stats = stats[0:self.OUTPUT_NUMBER].append(stats[-self.OUTPUT_NUMBER:]).reset_index(drop=True)
-        return stats[abs(stats['rate']) > self.OUTPUT_THRESHOLD].reset_index(drop=True)
+        # save max_utc as local time
+        self.rate_time = self.convert_timezone(max_utc)
 
-    def create_report(self, stats):
+        # filter by future names
+        if self.LIST_OF_FUTURES[0].lower() != 'all':
+            rates = rates[rates['future'].isin(LIST_OF_FUTURES)]
 
-        report = '{timestamp}\n'.format(
-            timestamp=datetime.fromtimestamp(self.ts).strftime('%Y-%m-%d - %H:%M:%S'))
+        # filter by number of futures
+        if len(rates) > 2 * self.OUTPUT_NUMBER:
+            rates = rates.reset_index(drop=True)
+            rates = rates[0:self.OUTPUT_NUMBER].append(rates[-self.OUTPUT_NUMBER:])
+
+        # filter by rate threshold
+        rates = rates[abs(rates['rate']) > self.OUTPUT_THRESHOLD]
+
+        return rates.reset_index(drop=True)
+
+    def create_report(self, rates):
+
+        report = '[{timestamp}]\n'.format(
+            timestamp=self.rate_time.strftime("%Y-%m-%d - %H:%M:%S"))
         
         report += 'Top {X}:\n'.format(X=self.OUTPUT_NUMBER)
         for i in range(self.OUTPUT_NUMBER):
-            report += '{fut} ({rate})\n'.format(
-                fut=stats.loc[i, 'future'], rate=stats.loc[i, 'rate'])
+            report += '{fut} ({rate:.6f})\n'.format(
+                fut=rates.loc[i, 'future'], rate=rates.loc[i, 'rate'])
         
         report += 'Bottom {X}:\n'.format(X=self.OUTPUT_NUMBER)
         for i in range(self.OUTPUT_NUMBER):
-            report += '{fut} ({rate})\n'.format(
-                fut=stats.loc[-self.OUTPUT_NUMBER+i, 'future'], rate=stats.loc[-self.OUTPUT_NUMBER+i, 'rate'])
+            report += '{fut} ({rate:.6f})\n'.format(
+                fut=rates.loc[len(rates)-1-i, 'future'], rate=rates.loc[len(rates)-1-i, 'rate'])
 
         return report
 
     def post_report(self, report):
-
         if self.tg_client != None:
             self.tg_client.send_message(chat_id=self.TELEGRAM_CHAT_ID, text=report)
         else:
             logger.info(report)
 
+    def convert_timezone(self, utc: str):
+        from_zone = tz.tzutc()
+        to_zone = tz.tzlocal()
+        utc = datetime.strptime(utc, '%Y-%m-%dT%H:%M:%S%z')
+        utc = utc.replace(tzinfo=from_zone)
+        return utc.astimezone(to_zone)
 
 if __name__ == '__main__':
     bot = Bot()
